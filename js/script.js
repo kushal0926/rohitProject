67fc4d9c885c1dab936c83b3740d45a2843af240
kushal tamang <kushaltamang0926@gmail.com>
2026-01-27 16:15:12 +0545

    Add JavaScript functionality for search, cart, and user buttons

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const search_btn = document.getElementById("search-btn");
+const cart_btn = document.getElementById("cart-btn");
+const user_btn = document.getElementById("user-btn");
+
+search_btn.addEventListener("click", () => {});
+
+cart_btn.addEventListener("click", () => {
+  window.location.href = "cart.html";
+});
+
+user_btn.addEventListener("click", () => {
+  window.location.href = "user.html";
+});
